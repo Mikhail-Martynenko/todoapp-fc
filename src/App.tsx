e1a4b23a3c5453f9a4fb4316948f7ee294bd6330
Mikhail Martynenko <misha.martynenko8@yandex.ru>
2023-05-12 18:18:26 +0300

    108002 feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const URL_PATH: string = `https://jsonplaceholder.typicode.com/posts/`
+
+interface ITasks {
+    tasks: {
+        id: number;
+        title: string;
+        isComplete: boolean;
+    }[];
+}
+
+const App: React.FC = () => {
+
+    const [tasksState, setStateTasks] = useState<ITasks>({
+        tasks: []
+    })
+
+    useEffect((): void => {
+        try {
+            (async () => {
+                const response = await fetch(URL_PATH);
+                if (response.ok) {
+                    const tasks = await response.json();
+                    console.log('Запрос прошёл успешно!')
+                    setStateTasks(() => ({
+                        tasks: [...tasks.slice(0, 5)]
+                    }));
+                }
+            })()
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
+
+    const addTaskEnter = (title: string) => {
+        const newTask = {
+            id: tasksState.tasks.length + 1,
+            title,
+            isComplete: false
+        };
+        setStateTasks((prevState) => ({tasks: [...prevState.tasks, newTask]}));
+    }
+
+    return (
+        <div className="App">
+            <h1>Task List</h1>
+            <input type="text" placeholder="Enter task name" onKeyDown={() => addTaskEnter} />
+            <div>{tasksState.tasks.map((task) =>
+                <div key={task.id}>
+                    <input type="checkbox" checked={task.isComplete} />
+                    <span style={{textDecoration: task.isComplete ? 'line-through' : 'none'}}>{task.title}</span>
+                    <button>Delete</button>
+                    <button>Edit</button>
+                </div>
+            )}</div>
+        </div>
+    );
 }
 
 export default App;
